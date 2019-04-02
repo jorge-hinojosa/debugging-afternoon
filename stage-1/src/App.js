6f25b6d029d9d809e17396b83cc147057bce3933
Jorge Hinojosa <jorgexhinojosa@gmail.com>
2019-04-02 12:27:55 -0500

    Fixed bugs in Stage 1

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -28,6 +29,7 @@ class App extends Component {
     this.setState({
       cart: [...this.state.cart, item]
     });
+    alert('Added to cart!')
   }
   removeFromCart(index) {
     let cartCopy = this.state.cart.slice();
@@ -54,9 +56,9 @@ class App extends Component {
         <NavBar navigate={this.navigate} />
         <div className="main-container">
           {showCart ? (
-            <ShoppingCart cart={cart} />
+            <ShoppingCart removeFromCart={this.removeFromCart} cart={this.state.cart} />
           ) : (
-            <StoreFront products={products} />
+            <StoreFront addToCart={this.addToCart} products={this.state.products} />
           )}
         </div>
       </div>
